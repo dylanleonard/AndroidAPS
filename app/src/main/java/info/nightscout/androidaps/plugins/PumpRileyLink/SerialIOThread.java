cbeea9b65a07b6e290d4c1d0665e1cdd84bcaa81
Dylan Leonard <dylan.a.leonard@gmail.com>
2017-09-18 00:45:54 -0400

    Added SerialIOTread for RileyLink plugin

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpRileyLink/SerialIOThread.java
@@ -0,0 +1,10 @@
+package info.nightscout.androidaps.plugins.PumpRileyLink;
+
+/**
+ * Created by Dylan on 9/17/2017.
+ */
+
+// ToDo: adapt serialiothread from DanaR
+
+public class SerialIOThread {
+}
